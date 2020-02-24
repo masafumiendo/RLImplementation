3f0bdb2f3965b9ff05c2003dd693403f7dce3f05
Masafumi Endo <endoma@oregonstate.edu>
2020-02-24 11:34:11 -0800

    fix a bug

--- access_colab.sh
+++ access_colab.sh
@@ -3,6 +3,6 @@
 for i in `seq 0 12`
 do
   echo "[$i]" ` date '+%y/%m/%d %H:%M:%S'` "connected."
-  open <<https://colab.research.google.com/drive/1M7gKDT7gJ9_tn4Ko3v8L2z804YxaPXd4#scrollTo=mfeUnZgmI5dA>>
+  open https://colab.research.google.com/drive/1M7gKDT7gJ9_tn4Ko3v8L2z804YxaPXd4#scrollTo=mfeUnZgmI5dA
   sleep 3600
 done
